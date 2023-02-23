7c16dca17c9152b817a3da995c476e42a7c66b17
TALANDIER Claude <ctalandi@ifremer.fr>
2023-02-23 17:47:24 +0100

    Add the final step of BERING data which consist in east-west flip at least and few other things

---
+++ INTERPOLATION/GLORYS12V1/BERING/FINAL/launch.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xv 
+
+SY=2022
+EY=2022
+
+
+if [ ! -d JOBS ] ; then mkdir -p JOBS  ; fi
+
+for cyear in `seq $SY $EY ` ; do 
+
+	#for gtype in `echo gridT gridS gridU gridV ` ; do 
+	#for gtype in `echo gridT gridS gridU gridV grid2D icemod` ; do 
+	for gtype in `echo icemod` ; do 
+
+		sed -e "s/<XXYEARXX>/${cyear}/"  -e "s/<XXVARGRDXX>/${gtype}/"  make_final_Bering_bdys.slurm.tmp > ./JOBS/make_final_Bering_bdys_y${cyear}_${gtype}.slurm
+		
+		# Must be submitted interactivelyy else it crashes (NCO command)
+                chmod 750 ./JOBS/make_final_Bering_bdys_y${cyear}_${gtype}.slurm
+		./JOBS/make_final_Bering_bdys_y${cyear}_${gtype}.slurm
+
+		#qsub ./JOBS/make_final_Bering_bdys_y${cyear}_${gtype}.slurm
+
+	done
+done 
