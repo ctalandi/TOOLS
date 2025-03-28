e718138595f0c7a805f7b76872dc8c7220b5bd62
TALANDIER Claude <ctalandi@ifremer.fr>
2025-03-28 14:21:20 +0100

    Add scripts used to manage data from MIO, download and BDYs shaping

---
+++ OTHERS/FREEBIORYS4V2/get_FREEBIORYS4V2_dataOPenDAP.py
@@ -0,0 +1,83 @@
+#!/bin/python
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+my_year=1992
+
+# Define all areas
+area_bering={'AREA':'BERING', 'lon_min' : -173., 'lon_max': -165.,'lat_min': 65., 'lat_max': 67.} 
+area_subtro={'AREA':'SUBTROPGYRE', 'lon_min' : -85., 'lon_max': -5.,'lat_min': 23., 'lat_max': 30.} 
+area_foldcr={'AREA':'FOLD-CREG025', 'lon_min' : -180., 'lon_max': +180.,'lat_min': 23., 'lat_max': 90.} 
+
+#bgcvar_lst=['alk', 'bfe', 'bgc2D', 'caco3', 'chl', 'dchl', 'dfe', 'dic', 'doc', 'dsi', 'fe', 'goc', 'gsi', \
+#           'nchl', 'nfe', 'nh4', 'no3', 'nppv', 'o2', 'par', 'ph', 'phy2', 'phyc', 'phy', 'po4', 'poc', 'sfe', 'si', 'zoo2', 'zoo']
+
+bgcvar_lst=['alk', 'bfe', 'caco3', 'chl', 'dchl', 'dfe', 'dic', 'doc', 'dsi', 'fe', 'goc', 'gsi', \
+           'nchl', 'nfe', 'nh4', 'no3', 'nppv', 'o2', 'par', 'phy2', 'phyc', 'phy', 'po4', 'poc', 'sfe', 'si', 'zoo2', 'zoo']
+
+lst_dom=[area_bering,area_subtro]
+#lst_dom=[area_bering,area_subtro,area_foldcr]
+
+# Loop on the area 
+for dom_extra in lst_dom: 
+
+    print(' The Sub-area considered is:', dom_extra['AREA'])
+
+    # Loop on variables 
+    for var in bgcvar_lst:
+    
+        # Read data base
+        datapgn = xr.open_dataset('https://tds.mercator-ocean.fr/thredds/dodsC/freebiorys2v4-monthly-'+var)
+        print()
+        print('>>>>>>>  The current data read is: ', var)
+        
+        geotest = (datapgn.nav_lon > dom_extra['lon_min']) & (datapgn.nav_lon < dom_extra['lon_max']) & (datapgn.nav_lat > dom_extra['lat_min']) & (datapgn.nav_lat < dom_extra['lat_max'])
+        geoindex = np.argwhere(geotest.values)
+        xmin = min(geoindex[:,1])
+        xmax = max(geoindex[:,1])
+        ymin = min(geoindex[:,0])
+        ymax = max(geoindex[:,0])
+        
+        ldate = pd.date_range(start=str(my_year)+"0101",end=str(my_year)+"1231",freq="M") # all monday between start and end
+        extra_data = datapgn.isel({'x':slice(xmin,xmax),'y':slice(ymin,ymax)}).sel({'time_counter':ldate},method="nearest")
+        
+        CONFIG='FREEBIORYS2V4'
+        OUTDIR='/data0/project/drakkar/CONFIGS/ORCA025.L75/FREEBIORYS4V2/INIT/'
+        extra_data.to_netcdf(OUTDIR+dom_extra['AREA']+'/'+str(my_year)+'/'+CONFIG+'_'+dom_extra['AREA']+'_1m_y'+str(my_year)+'_'+var+'.nc')
+
+
+# Meaning of all variables 
+#   alk:long_name = "Total Alkalinity" ;
+#   bfe:long_name = "Iron in the big particles" ;
+#   bgc2D: long_name = 
+#   caco3:long_name = "Calcite" ;
+#   chl:long_name = "Total Chlorophyll" ;
+#   dchl:long_name = "Chlorophyll of the Diatoms" ;
+#   dfe:long_name = "Iron content of the diatoms" ;
+#   dic:long_name = "Dissolved Inorganic Carbon" ;
+#   doc:long_name = "Dissolved Organic Carbon" ;
+#   dsi:long_name = "Silicon content of the Diatoms" ;
+#   fe:long_name = "Dissolved Iron" ;
+#   goc:long_name = "Big Particulate Organic Carbon" ;
+#   gsi:long_name = "Sinking biogenic Silica" ;
+#   nchl:long_name = "Chlorophyll of the Nanophytoplankton" ;
+#   nfe:long_name = "Iron content of the Nanophytoplankton" ;
+#   nh4:long_name = "Ammonium" ;
+#   no3:long_name = "Nitrate" ;
+#   nppv:long_name = "Total Primary Production of Phyto" ;
+#   o2:long_name = "Dissolved Oxygen" ;
+#   par:long_name = "Photosynthetically Available Radiation" ;
+#   ph:long_name = >> is a wrong variable which refers to diatoms which corresponds to variable phy2 below
+#   phy2:long_name = "Diatoms"
+#   phyc:long_name = "Total Phytoplankton"
+#   phy:long_name = "Nanophytoplankton"
+#   po4:long_name = "Phosphate"
+#   poc:long_name = "Small Particulate Organic Carbon"
+#   sfe:long_name = "Iron in the small particles"
+#   si:long_name = "Dissolved Silicate"
+#   zoo2:long_name = "Mesozooplankton" 
+#   zoo:long_name = "Microzooplankton" 
+#   
+#   
