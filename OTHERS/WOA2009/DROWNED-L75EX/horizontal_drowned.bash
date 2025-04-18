11e027eace2def3ab794bf47487a6270290be2b3
TALANDIER Claude <ctalandi@ifremer.fr>
2025-04-18 11:08:28 +0200

    Update script as namelists as well to perform a vertical interpolation only using a more recent SOSIE release

--- OTHERS/WOA2009/DROWNED-L75EX/horizontal_drowned.bash
+++ OTHERS/WOA2009/DROWNED-L75EX/horizontal_drowned.bash
@@ -7,10 +7,12 @@ set -x
 #    over land areas using sosie package
 
 res=1 # 1 is the one available resolution
+smo=1 # Smoothing level 
 
 cd OPERATE
-ln -sf ../sosie.x .
-ln -sf ../mask_field.nc .
+# Rely on the Sosie release 3.0 (Git Master) version (important for the namelist)
+ln -sf /home1/datahome/ctalandi/DEV/SOSIE_GIT_MASTER/bin/sosie3.x .
+ln -sf ../woa09_3Dmsk_1deg.nc  mask_field.nc 
 ln -sf ../deptht_L75.nc .
 
 for var in `echo salinity temperature ` ; do
@@ -21,11 +23,11 @@ for var in `echo salinity temperature ` ; do
            svar='salinity'
            namv='s_an'
         fi
-	ln -sf ../namelist_woa09_${namv}_drownfields .
-        ln -sf ../COMBINED/woa09_${svar}_monthly_${res}deg_${namv}_CMA.nc input_field.nc
+	ln -sf ../namelist_woa09_${namv}_drownfields_soGitMaster .
+        ln -sf ../DATA/woa09_${svar}_monthly_${res}deg_${namv}_CMA_SM${smo}_4So.nc input_field.nc
 
-	./sosie.x -f namelist_woa09_${namv}_drownfields
-	mv ${namv}_WOA09-360x180_drowned.nc ../CMA_DROWNED/woa09_${svar}_monthly_${res}deg_${namv}_CMA_drowned.nc 
+	./sosie3.x -f namelist_woa09_${namv}_drownfields_soGitMaster
+	mv ${namv}_WOA09-Z75_WOA09_360x180_drowned.nc ../CMA_DROWNED/woa09_${svar}_monthly_${res}deg_${namv}_CMA_SM${smo}_drowned.nc 
 
 done
 
